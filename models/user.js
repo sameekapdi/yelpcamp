1e6708097fc74fb3376dd4318a60257569c7afe4
Samee Kapdi <sameekapdi@gmail.com>
2019-03-08 00:24:02 +0000

    added password reset

--- models/user.js
+++ models/user.js
@@ -2,12 +2,14 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var userSchema = new mongoose.Schema({
-    username: String,
+    username: {type: String, unique: true, required: true},
     password: String,
+    email: {type: String, unique: true, required: true},
     avatar: {type: String, default:"https://i.imgur.com/vhEWUeu.jpg"},
     firstName: String,
     lastName: String,
-    email: String,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     isAdmin: {type: Boolean, default: false}
 });
 
